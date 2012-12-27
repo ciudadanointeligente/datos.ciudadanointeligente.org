d4ab2b1bbbb1d1219eca8188f106805bf1969042
Felipe Álvarez <luisfelipealvarez@gmail.com>
2012-12-27 13:13:30 -0300

    Fixing problems with new version of ckan

--- ckanext/dfci/controllers.py
+++ ckanext/dfci/controllers.py
@@ -14,7 +14,5 @@ class CustomHomeController(HomeController):
 	def index(self):
 		response = super(CustomHomeController, self).index()
 		context = {'model': model, 'session': model.Session, 'user': c.user or c.author}
-		c.recently_changed_packages_activity_list_html = \
-				ckan.logic.action.get.recently_changed_packages_activity_list_html(context, {})
 		return render('home/index.html', cache_force=False)
 
