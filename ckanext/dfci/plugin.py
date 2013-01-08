cbe019f06e535f7e5d519ee838bd4cbaf599f90b
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-01-08 12:20:54 -0300

    Fixing bug in address of assets

--- ckanext/dfci/plugin.py
+++ ckanext/dfci/plugin.py
@@ -20,9 +20,9 @@ class DFCIPlugin(SingletonPlugin):
 		config['extra_public_paths'] = ','.join([our_public_dir,config.get('extra_public_paths', '')])
 		config['extra_template_paths'] = ','.join([template_dir,config.get('extra_template_paths', '')])
 		config['ckan.site_title'] = u"Datos de la Fundacion Ciudadano Inteligente"
-		config['ckan.site_logo'] = "base/images/logo-data.png"
+		config['ckan.site_logo'] = "/base/images/logo-data.png"
         #ckan.favicon = /images/icons/ckan.ico
-		config['ckan.favicon'] = "base/images/icons/favicon-data.ico"
+		config['ckan.favicon'] = "/base/images/icons/favicon-data.ico"
 
 
 
