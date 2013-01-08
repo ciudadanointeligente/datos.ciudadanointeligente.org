0b6a93ca96bd188e4449ad35e202bcce50e851bc
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-01-08 12:08:27 -0300

    Adding logo to configuration

--- ckanext/dfci/plugin.py
+++ ckanext/dfci/plugin.py
@@ -20,6 +20,7 @@ class DFCIPlugin(SingletonPlugin):
 		config['extra_public_paths'] = ','.join([our_public_dir,config.get('extra_public_paths', '')])
 		config['extra_template_paths'] = ','.join([template_dir,config.get('extra_template_paths', '')])
 		config['ckan.site_title'] = u"Datos de la Fundacion Ciudadano Inteligente"
+		config['ckan.site_logo'] = "base/images/logo-data.png"
 
 
 
